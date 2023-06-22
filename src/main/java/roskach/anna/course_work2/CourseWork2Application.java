4cd7c522aba95051338955eef45c2e4d581483cb
annaroskach <danuta_tyt@mail.ru>
2023-06-22 14:36:27 +0300

    cw2

--- src/main/java/roskach/anna/course_work2/CourseWork2Application.java
+++ src/main/java/roskach/anna/course_work2/CourseWork2Application.java
@@ -3,6 +3,7 @@ package roskach.anna.course_work2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CourseWork2Application {
 
